e8af4def42d4a4e0fbb0847c8afb7d2caa1f1e56
heekwon <seoheekwon@gmail.com>
2018-02-04 05:29:19 +0000

    create group page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import {GroupInfoPage} from '../group-info/group-info';
+import {CreateGroupPage} from '../create-group/create-group';
+
 
 @Component({
   selector: 'page-home',
@@ -8,7 +10,7 @@ import {GroupInfoPage} from '../group-info/group-info';
 })
 export class HomePage {
 
-  constructor(private modal: ModalController) {
+  constructor(private modal: ModalController, public navCtrl: NavController) {
 
   }
   
@@ -16,5 +18,10 @@ export class HomePage {
     const myModal =this.modal.create('GroupInfoPage')
     myModal.present();
 }
+
+    createGroup(){
+        this.navCtrl.push('CreateGroupPage');
+               
+    }
 
 }
